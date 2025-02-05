4bf7a6b0def2061648591bd662014ef25acf6720
RizkiNiih <rizkigaming0204@gmail.com>
2025-02-05 22:34:28 +0700

    Learn-Laravel

--- Belajar_Laravel8/resources/views/dashboard/layouts/sidebar.blade.php
+++ Belajar_Laravel8/resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,36 +1,52 @@
 <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+  <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+          <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">
-              <svg class="bi"><use xlink:href="#house-fill"/></svg>
-              Dashboard
-            </a>
-          </li> 
-          <li class="nav-item">
-            <a class="nav-link {{ Request::is('dashboard/posts*') ? 'active' : '' } d-flex align-items-center gap-2" href="/dashboard/posts">
-              <i class="bi bi-file-earmark-text"></i>
-              My Posts
-            </a>
-          </li>
-        </ul>
+          <ul class="nav flex-column">
+              <li class="nav-item">
+                  <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">
+                      <svg class="bi">
+                          <use xlink:href="#house-fill"/>
+                      </svg>
+                      Dashboard
+                  </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
+                      <i class="bi bi-file-earmark-text"></i>
+                      My Posts
+                  </a>
+              </li>
+          </ul>
 
 
-        <hr class="my-3">
+          @can('admin')
+          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>ADMINISTRATOR</span>
+          </h6>
+          <ul class="nav flex-colomn">
+            <li class="nav-item"> <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+              <i class="bi bi-grid mb-1"></i>
+              Post categories
+          </a></li>
+          </ul>
+          @endcan
 
-        <ul class="nav flex-column mb-auto">
-          <li class="nav-item">
-              <form action="/logout" method="post">
-                  @csrf
-                  <button type="submit" class="nav-link d-flex align-items-center px-3 border-0" onclick="return confirm('Are you sure?')">Logout <i class="bi bi-box-arrow-in-right mb-1"></i></a></li></button>
-                </form>
-          </li>
-        </ul>
+          <hr class="my-3">
+
+          <ul class="nav flex-column mb-auto">
+              <li class="nav-item">
+                  <form action="/logout" method="post">
+                      @csrf
+                      <button type="submit" class="nav-link d-flex align-items-center px-3 border-0" onclick="return confirm('Are you sure?')">
+                          Logout <i class="bi bi-box-arrow-in-right mb-1"></i>
+                      </button>
+                  </form>
+              </li>
+          </ul>
       </div>
-    </div>
   </div>
+</div>
