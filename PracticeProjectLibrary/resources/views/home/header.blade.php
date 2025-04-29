cbe6969faca506b386c931a9a996ff1d6ee9a41e
RizkiNiih <rizkigaming0204@gmail.com>
2025-04-29 17:33:53 +0700

    Fix in Practice Project Libary

--- PracticeProjectLibrary/resources/views/home/header.blade.php
+++ PracticeProjectLibrary/resources/views/home/header.blade.php
@@ -10,11 +10,8 @@
                     <!-- ***** Logo End ***** -->
                     <!-- ***** Menu Start ***** -->
                     <ul class="nav">
-                        <li><a href="index.html" class="active">Home</a></li>
+                        <li><a href="{{ url('/') }}" class="active">Home</a></li>
                         <li><a href="{{ url('explore') }}">Explore</a></li>
-                        <li><a href="details.html">Item Details</a></li>
-                        <li><a href="author.html">Author</a></li>
-                        <li><a href="create.html">Create Yours</a></li>
 
 
                         @if (Route::has('login'))
