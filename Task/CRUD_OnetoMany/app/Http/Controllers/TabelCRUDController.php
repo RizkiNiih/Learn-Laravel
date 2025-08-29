f85043d4ac007e67df89552ea5d677ef652b06fa
RizkiNiih <rizkigaming0204@gmail.com>
2025-08-29 17:56:39 +0700

    Merapikan file

---
+++ Task/CRUD_OnetoMany/app/Http/Controllers/TabelCRUDController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class TabelCRUDController extends Controller
+{
+    public function home()
+    {
+        $postingan = Post::with('commment')->get();
+        return view('berita.home', compact('postingan'));
+    }
+
+    public function Untukanda()
+    {
+        $postingan = Post::with('commment')->get();
+        return view('berita.Untukanda', compact('postingan'));
+    }
+}
--- Task/CRUD_OnetoMany/routes/web.php
+++ Task/CRUD_OnetoMany/routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\TabelCRUDController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/Untukanda', function () {
-    return view('Untukanda');
+Route::prefix('berita')->name('berita.')->group(function () {
+    Route::get('/home', [TabelCRUDController::class, 'home'])->name('home');
+    Route::get('/Untukanda', [TabelCRUDController::class, 'Untukanda'])->name('Untukanda');
+    Route::get('/create', [TabelCRUDController::class, 'create'])->name('create');
+    Route::post('/store', [TabelCRUDController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [TabelCRUDController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [TabelCRUDController::class, 'update'])->name('update');
+    Route::delete('/delete/{id}', [TabelCRUDController::class, 'destroy'])->name('destroy');
 });
